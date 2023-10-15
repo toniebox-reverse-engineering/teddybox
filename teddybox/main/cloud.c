b8b05d2140796af0eab68fe2264c86fdb347dbf6
g3gg0 <git@g3gg0.de>
2023-10-15 23:11:30 +0200

    continue work on cloud request code

--- teddybox/main/cloud.c
+++ teddybox/main/cloud.c
@@ -34,48 +34,79 @@ size_t client_der_len = 0;
 size_t private_der_len = 0;
 static esp_app_desc_t running_app_info;
 
-esp_err_t http_body_decode(void *ctx, uint8_t *data, size_t length)
+esp_err_t http_parser_handler(void *ctx, uint8_t *data, size_t length)
 {
-    http_wrapper_ctx_t *wrapper_ctx = (http_wrapper_ctx_t *)ctx;
+    esp_err_t ret = ESP_OK;
+    http_parser_t *parser_ctx = (http_parser_t *)ctx;
 
-    if (!wrapper_ctx->header_parsed)
+    if (!parser_ctx->header_parsed)
     {
-        if (wrapper_ctx->http_header_size + length > MAX_HTTP_HEADER_SIZE)
+        if (parser_ctx->header_size + length > MAX_HTTP_HEADER_SIZE)
         {
+            ESP_LOGI(TAG, "Buffer overflow");
             return ESP_FAIL;
         }
-        memcpy(wrapper_ctx->http_header_buffer + wrapper_ctx->http_header_size, data, length);
-        wrapper_ctx->http_header_size += length;
 
-        char *header_end = strstr((char *)wrapper_ctx->http_header_buffer, "\r\n\r\n");
+        memcpy(parser_ctx->header_buffer + parser_ctx->header_size, data, length);
+        parser_ctx->header_size += length;
+
+        char *header_end = strstr((char *)parser_ctx->header_buffer, "\r\n\r\n");
         if (header_end)
         {
-            char *content_length_str = strstr((char *)wrapper_ctx->http_header_buffer, "Content-Length: ");
-            if (content_length_str)
+            ESP_LOGI(TAG, "Found end of header");
+
+            int status_code;
+            if (sscanf((char *)parser_ctx->header_buffer, "HTTP/1.1 %d ", &status_code) == 1)
             {
-                sscanf(content_length_str, "Content-Length: %zu", &wrapper_ctx->content_length);
+                ESP_LOGI(TAG, "Parsed HTTP Status Code: %d", status_code);
+                if (parser_ctx->http_status_cbr)
+                {
+                    ret = parser_ctx->http_status_cbr(parser_ctx->ctx, status_code);
+                }
             }
 
-            wrapper_ctx->header_parsed = 1;
+            char *content_length_str = strstr((char *)parser_ctx->header_buffer, "Content-Length: ");
+            if (content_length_str)
+            {
+                sscanf(content_length_str, "Content-Length: %zu", &parser_ctx->content_length);
+                ESP_LOGI(TAG, "Parsed Content-Length: %zu", parser_ctx->content_length);
+
+                if (parser_ctx->content_length_cbr)
+                {
+                    parser_ctx->content_length_cbr(parser_ctx->ctx, parser_ctx->content_length);
+                }
+            }
+            parser_ctx->header_parsed = 1;
 
             uint8_t *content_start = (uint8_t *)(header_end + 4);
-            size_t content_size = wrapper_ctx->http_header_size - (content_start - wrapper_ctx->http_header_buffer);
+            size_t content_size = parser_ctx->header_size - (content_start - parser_ctx->header_buffer);
 
-            if (wrapper_ctx->original_cbr)
+            if (parser_ctx->http_data_cbr)
             {
-                return wrapper_ctx->original_cbr(wrapper_ctx->original_ctx, content_start, content_size);
+                ret = parser_ctx->http_data_cbr(parser_ctx->ctx, content_start, content_size);
             }
         }
     }
     else
     {
-        if (wrapper_ctx->original_cbr)
+        parser_ctx->received_data_length += length;
+
+        if (parser_ctx->http_data_cbr)
         {
-            return wrapper_ctx->original_cbr(wrapper_ctx->original_ctx, data, length);
+            ret = parser_ctx->http_data_cbr(parser_ctx->ctx, data, length);
+        }
+
+        if (parser_ctx->received_data_length >= parser_ctx->content_length)
+        {
+            if (parser_ctx->http_end_cbr)
+            {
+                parser_ctx->http_end_cbr(parser_ctx->ctx);
+            }
+            ret = CLOUD_SUCCESS_DISCONNECT;
         }
     }
 
-    return ESP_OK;
+    return ret;
 }
 
 esp_err_t cloud_load_cert(const char *path, uint8_t **ptr, size_t *length)
@@ -141,6 +172,7 @@ static esp_err_t cloud_request(cloud_req_t *req)
         .clientkey_bytes = private_der_len,
         .skip_common_name = true};
 
+    uint8_t *receive_buffer = malloc(HTTP_RECEIVE_SIZE);
     struct esp_tls *tls = esp_tls_conn_http_new(url, &cfg);
 
     if (!tls)
@@ -172,9 +204,7 @@ static esp_err_t cloud_request(cloud_req_t *req)
 
     while (connected)
     {
-        uint8_t buf[513] = {0};
-
-        int len = esp_tls_conn_read(tls, buf, sizeof(buf) - 1);
+        int len = esp_tls_conn_read(tls, receive_buffer, HTTP_RECEIVE_SIZE);
 
         if (len == ESP_TLS_ERR_SSL_WANT_WRITE || len == ESP_TLS_ERR_SSL_WANT_READ)
         {
@@ -204,14 +234,13 @@ static esp_err_t cloud_request(cloud_req_t *req)
             break;
         }
 
-        ESP_LOGI(TAG, "%d bytes received", len);
         if (req->data_received_cbr)
         {
-            connected = (req->data_received_cbr(req->data_received_ctx, buf, len) == ESP_OK);
+            connected = (req->data_received_cbr(req->data_received_ctx, receive_buffer, len) == ESP_OK);
         }
         else
         {
-            ESP_LOGI(TAG, "'%s'", buf);
+            ESP_LOGI(TAG, "%d bytes received", len);
         }
         ret = ESP_OK;
     }
@@ -222,6 +251,7 @@ exit:
     free(auth_line);
     free(request);
     free(url);
+    free(receive_buffer);
 
     return ret;
 }
@@ -268,32 +298,122 @@ esp_err_t cloud_set_time_cbr(void *ctx, uint8_t *data, size_t length)
 
 esp_err_t cloud_set_time(void)
 {
-    static http_wrapper_ctx_t http_body_decode_ctx = {
-        .original_cbr = &cloud_set_time_cbr};
-    static cloud_req_t req = {
+    http_parser_t http_parser_handler_ctx = {
+        .http_data_cbr = &cloud_set_time_cbr};
+    cloud_req_t req = {
         .host = "192.168.1.26",
         .port = 443,
         .path = "/v1/time",
-        .auth = NULL,
-        .data_received_cbr = &http_body_decode,
-        .data_received_ctx = &http_body_decode_ctx};
+        .data_received_cbr = &http_parser_handler,
+        .data_received_ctx = &http_parser_handler_ctx};
+
+    return cloud_request(&req);
+}
+
+typedef struct
+{
+    uint32_t status_code;
+    uint32_t content_length;
+    uint32_t received;
+} cloud_content_ctx_t;
+
+esp_err_t cloud_content_status_cbr(void *ctx_in, int status_code)
+{
+    cloud_content_ctx_t *ctx = (cloud_content_ctx_t *)ctx_in;
+    ctx->status_code = status_code;
+    ESP_LOGI(TAG, "cloud_content_status_cbr: '%d'", status_code);
+    return ESP_OK;
+}
+
+esp_err_t cloud_content_length_cbr(void *ctx_in, size_t content_length)
+{
+    cloud_content_ctx_t *ctx = (cloud_content_ctx_t *)ctx_in;
+    ctx->content_length = content_length;
+    ESP_LOGI(TAG, "%d bytes total", ctx->content_length);
+    return ESP_OK;
+}
+
+esp_err_t cloud_content_cbr(void *ctx_in, uint8_t *data, size_t length)
+{
+    cloud_content_ctx_t *ctx = (cloud_content_ctx_t *)ctx_in;
+    ctx->received += length;
+
+    time_t current = time(NULL);
+    static time_t start = 0;
+    static time_t last = 0;
+
+    if (start == 0)
+    {
+        start = current;
+    }
+
+    if (last != current)
+    {
+        last = current;
+
+        float elapsed = difftime(current, start);
+        float speed = (ctx->received / elapsed) / 1024.0; // KiB/s
+        float percent_complete = (ctx->received * 100.0f / ctx->content_length);
+        float eta = ((ctx->content_length - ctx->received) / (ctx->received / elapsed)); // ETA in seconds
+
+        ESP_LOGI(TAG, "%d bytes received (%2.2f%%), Speed: %2.2f KiB/s, ETA: %2.2f s",
+                 ctx->received, percent_complete, speed, eta);
+    }
+    return ESP_OK;
+}
+
+esp_err_t cloud_content_end_cbr(void *ctx_in)
+{
+    cloud_content_ctx_t *ctx = (cloud_content_ctx_t *)ctx_in;
+    ESP_LOGI(TAG, "cloud_content_end_cbr %d/%d", ctx->received, ctx->content_length);
+    return ESP_OK;
+}
+
+esp_err_t cloud_download(uint64_t uid)
+{
+    cloud_content_ctx_t cc_ctx = {0};
+    char filename[64];
+    char *filename_ptr = filename;
+
+    filename_ptr += sprintf(filename_ptr, "/v2/content/");
+
+    for (int i = 0; i < 8; ++i)
+    {
+        uint8_t byte = (uid >> (i * 8)) & 0xFF;
+        filename_ptr += sprintf(filename_ptr, "%02X", byte);
+    }
+
+    http_parser_t http_parser_handler_ctx = {
+        .http_status_cbr = &cloud_content_status_cbr,
+        .content_length_cbr = &cloud_content_length_cbr,
+        .http_data_cbr = &cloud_content_cbr,
+        .http_end_cbr = &cloud_content_end_cbr,
+        .ctx = &cc_ctx};
+
+    cloud_req_t req = {
+        .host = "192.168.1.26",
+        .port = 443,
+        .path = filename_ptr,
+        .auth = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+        .data_received_cbr = &http_parser_handler,
+        .data_received_ctx = &http_parser_handler_ctx};
 
     return cloud_request(&req);
 }
 
 static void cloud_task(void *ctx)
 {
-    bool time_set = false;
+    bool cloud_available = false;
     while (true)
     {
         vTaskDelay(100 / portTICK_PERIOD_MS);
 
-        if (!time_set)
+        if (!cloud_available)
         {
             ESP_LOGI(TAG, "Try to get time");
             if (cloud_set_time() == ESP_OK)
             {
-                time_set = true;
+                cloud_available = true;
             }
             else
             {
@@ -301,18 +421,12 @@ static void cloud_task(void *ctx)
             }
         }
 
-        if (0)
+        if (cloud_available)
         {
             vTaskDelay(10000 / portTICK_PERIOD_MS);
             ESP_LOGI(TAG, "Start request");
-            cloud_req_t req = {
-                .host = "192.168.1.26",
-                .port = 443,
-                .path = "/v2/content/00112233030405E0",
-                .auth = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
-                .data_received_cbr = NULL};
 
-            cloud_request(&req);
+            cloud_download(0xE0040350103E913B);
 
             ESP_LOGI(TAG, "Finish request");
         }
@@ -331,5 +445,5 @@ void cloud_init(void)
     cloud_load_cert("/spiflash/cert/client.der", &client_der, &client_der_len);
     cloud_load_cert("/spiflash/cert/private.der", &private_der, &private_der_len);
 
-    xTaskCreate(&cloud_task, "cloud_task", 8192, NULL, 5, NULL);
+    xTaskCreate(&cloud_task, "cloud_task", 8000, NULL, 5, NULL);
 }
--- teddybox/main/cloud.h
+++ teddybox/main/cloud.h
@@ -1,16 +1,23 @@
 #pragma once
 
 
-#define MAX_HTTP_HEADER_SIZE 1024 // Adjust this size as needed
+#define MAX_HTTP_HEADER_SIZE 1024
+#define HTTP_RECEIVE_SIZE 512
+
+#define CLOUD_SUCCESS_DISCONNECT -2
 
 typedef struct {
-    esp_err_t (*original_cbr)(void *ctx, uint8_t *data, size_t length);
-    void *original_ctx;
-    uint8_t http_header_buffer[MAX_HTTP_HEADER_SIZE];
-    size_t http_header_size;
+    esp_err_t (*http_status_cbr)(void *ctx, int status_code);
+    esp_err_t (*http_data_cbr)(void *ctx, uint8_t *data, size_t length);
+    esp_err_t (*http_end_cbr)(void *ctx);
+    esp_err_t (*content_length_cbr)(void *ctx, size_t content_length);
+    void *ctx;
     size_t content_length;
+    size_t received_data_length;
+    uint8_t header_buffer[MAX_HTTP_HEADER_SIZE];
+    size_t header_size;
     int header_parsed;
-} http_wrapper_ctx_t;
+} http_parser_t;
 
 
 typedef struct {
